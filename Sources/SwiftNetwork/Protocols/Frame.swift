7b6687a61988d813d429fc39c18828f9934619ed
agnosticdev <agnosticdev@gmail.com>
2026-07-28 16:34:53 -0500

    Removed unused code

--- Sources/SwiftNetwork/Protocols/Frame.swift
+++ Sources/SwiftNetwork/Protocols/Frame.swift
@@ -793,15 +793,6 @@ extension Frame {
 @available(Network 0.1.0, *)
 extension Frame {
 
-    //    @inline(__always)
-    //    func copyInto(inlineArray: inout [20 of UInt8], length: Int) {
-    //        guard startOffset + length <= self._bytes.count else {
-    //            return
-    //        }
-    //        for i in 0..<length {
-    //            inlineArray[i] = self._bytes[startOffset + i]
-    //        }
-    //    }
     // Copy length bytes from offset in this Frame into destination Frame.
     // checking the source offset, length and destination fit.
     // Return the length that it was able to copy into destination.
