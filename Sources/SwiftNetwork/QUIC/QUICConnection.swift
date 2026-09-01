8b5e79bf1aa3c8a3c9c5dd4f06a72f334d9c5beb
Raphael Hiesgen <r_hiesgen@apple.com>
2026-09-01 10:09:42 +0100

    Reset server token after validation

--- Sources/SwiftNetwork/QUIC/QUICConnection.swift
+++ Sources/SwiftNetwork/QUIC/QUICConnection.swift
@@ -1643,6 +1643,11 @@ public final class QUICConnection: ManyToManyApplicationStreamProtocol,
                 return
             }
 
+            if state == .retrySent {
+                // The token was validated and is no longer needed.
+                self.initialToken = nil
+            }
+
             // Change state from idle to initial received with key state = handshake
             state.change(to: .initialReceived, logIDString: logPrefixer.logIDString)
             keyState = .handshake
@@ -3763,7 +3768,7 @@ public final class QUICConnection: ManyToManyApplicationStreamProtocol,
                     sentPacketRecord: &sentPacketRecord,
                     connection: self,
                     availableCongestionWindow: availableCongestionWindow,
-                    token: isServer ? nil : initialToken,
+                    token: initialToken,
                     stats: &stats,
                     version: currentVersion,
                     isServer: isServer,
